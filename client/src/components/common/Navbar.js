4a702aaa84dfd9a5df39d8d6ee4bcc542fcdcb9b
toptalentcoder <kevinhart.dev@gmail.com>
2025-03-17 10:24:41 -0400

    add profile menu

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,11 +1,20 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Image from "next/image";
 import { RxPencil1 } from "react-icons/rx";
 import { PiCrownSimpleBold } from "react-icons/pi";
 import { IoIosArrowDown } from "react-icons/io";
 import { LuLink2 } from "react-icons/lu";
 import { MdAutoGraph } from "react-icons/md";
-import Image from "next/image";
+import { CgProfile } from "react-icons/cg";
+import { FaFolder } from "react-icons/fa";
+import { GoOrganization } from "react-icons/go";
+import { FaUsers } from "react-icons/fa";
+import { FaListCheck } from "react-icons/fa6";
+import { FaDatabase } from "react-icons/fa6";
+import { BiWallet } from "react-icons/bi";
+import { CiViewList, CiLogout } from "react-icons/ci";
+import { IoMdHelpCircleOutline } from "react-icons/io";
 
 const navigation = [
     { name: 'Dashboard', href: '#', current: true },
@@ -21,8 +30,8 @@ function classNames(...classes) {
 export default function Example() {
     return (
         <Disclosure as="nav" className="bg-[#413793]">
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-20 items-center ">
+            <div className=" max-w-full px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-[4.5rem] items-center ">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* Mobile menu button*/}
                         <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
@@ -427,56 +436,161 @@ export default function Example() {
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <button
-                        type="button"
-                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                        >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon aria-hidden="true" className="size-6" />
+                            type="button"
+                            className="relative rounded-full p-1 text-white hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                            >
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">View notifications</span>
+                            <BellIcon aria-hidden="true" className="size-6" />
                         </button>
 
                         {/* Profile dropdown */}
                         <Menu as="div" className="relative ml-3">
-                        <div>
-                            <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">Open user menu</span>
-                            <img
-                                alt=""
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                className="size-8 rounded-full"
-                            />
-                            </MenuButton>
-                        </div>
-                        <MenuItems
-                            transition
-                            className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-                        >
-                            <MenuItem>
-                            <a
-                                href="#"
-                                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                            <div>
+                                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                                <span className="absolute -inset-1.5" />
+                                <span className="sr-only">Open user menu</span>
+                                <img
+                                    alt=""
+                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    className="size-8 rounded-full"
+                                />
+                                </MenuButton>
+                            </div>
+                            <MenuItems
+                                transition
+                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
                             >
-                                Your Profile
-                            </a>
-                            </MenuItem>
-                            <MenuItem>
-                            <a
-                                href="#"
-                                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                            >
-                                Settings
-                            </a>
-                            </MenuItem>
-                            <MenuItem>
-                            <a
-                                href="#"
-                                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                            >
-                                Sign out
-                            </a>
-                            </MenuItem>
-                        </MenuItems>
+                                <MenuItem>
+                                    <span
+                                        className="block px-7 pt-4 text-[0.65rem] text-blue-500 data-focus:outline-hidden"
+                                    >
+                                        ACCOUNT-Paige Hintz Org
+                                    </span>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <CgProfile />
+                                        <a
+                                            href="#"
+                                        >
+                                            Profile
+                                        </a>
+                                    </div>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <FaFolder />
+                                        <a
+                                            href="#"
+                                        >
+                                            My guest guides
+                                        </a>
+                                    </div>
+                                </MenuItem>
+
+                                <div className='px-4 text-gray-300'>
+                                    <hr />
+                                </div>
+
+                                <MenuItem>
+                                    <span
+                                        className="block px-7 pt-4 text-[0.65rem] text-blue-500 data-focus:outline-hidden"
+                                    >
+                                        PAIGE HINTZ ORG
+                                    </span>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <GoOrganization />
+                                        <a
+                                            href="#"
+                                        >
+                                            Organization
+                                        </a>
+                                    </div>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <FaUsers />
+                                        <a
+                                            href="#"
+                                        >
+                                            Users
+                                        </a>
+                                    </div>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <FaListCheck />
+                                        <a
+                                            href="#"
+                                        >
+                                            Logs
+                                        </a>
+                                    </div>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <FaDatabase />
+                                        <a
+                                            href="#"
+                                        >
+                                            Plans
+                                        </a>
+                                    </div>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <BiWallet  />
+                                        <a
+                                            href="#"
+                                        >
+                                            Billing
+                                        </a>
+                                    </div>
+                                </MenuItem>
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <CiViewList  />
+                                        <a
+                                            href="#"
+                                        >
+                                            API
+                                        </a>
+                                    </div>
+                                </MenuItem>
+
+                                <div className='px-4 text-gray-300'>
+                                    <hr />
+                                </div>
+
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-5 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <IoMdHelpCircleOutline   />
+                                        <a
+                                            href="#"
+                                        >
+                                            Help
+                                        </a>
+                                    </div>
+                                </MenuItem>
+
+                                <div className='px-4 text-gray-300'>
+                                    <hr />
+                                </div>
+
+                                <MenuItem>
+                                    <div className='flex items-center space-x-2 px-7 py-5 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
+                                        <CiLogout   />
+                                        <a
+                                            href="#"
+                                        >
+                                            Log out
+                                        </a>
+                                    </div>
+                                </MenuItem>
+                            </MenuItems>
                         </Menu>
                     </div>
                 </div>
@@ -486,16 +600,16 @@ export default function Example() {
                 <div className="space-y-1 px-2 pt-2 pb-3">
                 {navigation.map((item) => (
                     <DisclosureButton
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
-                    className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'block rounded-md px-3 py-2 text-base font-medium',
-                    )}
-                    >
-                    {item.name}
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        aria-current={item.current ? 'page' : undefined}
+                        className={classNames(
+                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'block rounded-md px-3 py-2 text-base font-medium',
+                        )}
+                        >
+                        {item.name}
                     </DisclosureButton>
                 ))}
                 </div>
