657c6cc962a59a35a9d7d12bfba1fadee2614aca
BenjaminGrayDev <benjamin.gray.dev@gmail.com>
2025-03-18 16:34:36 -0400

    add plan db endpoint

--- server/src/endpoints/index.ts
+++ server/src/endpoints/index.ts
@@ -1,5 +1,7 @@
 import { googleAuthEndpoint } from "./auth/googleAuthEndpoint";
+import { getPlansFromDbEndpoint } from "./paypal/getPlansFromDbEndpoint";
 
 export const customEndpoints = [
-    googleAuthEndpoint
+    googleAuthEndpoint,
+    getPlansFromDbEndpoint
 ]
---
+++ server/src/endpoints/paypal/getPlansFromDbEndpoint.ts
@@ -0,0 +1,34 @@
+import { withErrorHandling } from "@/middleware/errorMiddleware";
+import { getPlansForSubscription } from "@/services/paypal/getPlansFromDbService";
+import { Endpoint, PayloadRequest } from "payload";
+
+export const getPlansFromDbEndpoint : Endpoint = {
+
+    path : '/plans',
+
+    method : 'get',
+
+    handler : withErrorHandling(async(req : PayloadRequest) => {
+        // Fetch the plans from your backend endpoint
+        const fetchedPlans = await getPlansForSubscription(req.payload);
+
+        // CORS headers
+        const corsHeaders = {
+            "Access-Control-Allow-Origin": "*", // You can replace '*' with specific domains for security reasons
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        };
+
+        // Return the array of plans
+        return new Response(
+            JSON.stringify({ fetchedPlans }),
+            {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...corsHeaders
+                },
+            }
+        );
+    })
+}
