44dcfeb32148fb3d7be2becc333dcab5e46ac672
toptalentcoder <kevinhart.dev@gmail.com>
2025-03-21 16:26:59 -0400

    add logic for default project seo guides

--- server/src/types/project.d.ts
+++ server/src/types/project.d.ts
@@ -1,6 +1,7 @@
 // ✅ Define a Project Interface for Type Safety
 export interface Project {
     projectID: string;
+    projectName : string;
     domainName: string;
     seoGuides: string[] | [];
 }
