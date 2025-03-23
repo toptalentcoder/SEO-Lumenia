1d8e6a619dc9d9c69e6da31e5209f8412dc92f19
BenjaminGrayDev <benjamin.gray.dev@gmail.com>
2025-03-23 04:14:31 -0400

    add endpoint for queiryID

---
+++ client/src/app/guide/[queryID]/page.js
@@ -0,0 +1,10 @@
+export default function GuidePage({ params }) {
+
+    const { queryID } = params;
+
+    return(
+        <div>
+            {queryID}
+        </div>
+    )
+};
--- client/src/app/guide/page.js
+++
@@ -1,8 +0,0 @@
-export default function GuidePage() {
-
-    return(
-        <div>
-            aaa
-        </div>
-    )
-};
