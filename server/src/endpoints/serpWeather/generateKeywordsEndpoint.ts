5a451713b47e57bcfddf1a3abb28e89d00aedf9c
toptalentcoder <kevinhart.dev@gmail.com>
2025-04-09 12:49:59 -0400

    add keywords collection
    Co-authored-by : HealerDevOps <gonggong624721@gmail.com>

---
+++ server/src/endpoints/serpWeather/generateKeywordsEndpoint.ts
@@ -0,0 +1,14 @@
+// import { withErrorHandling } from "@/middleware/errorMiddleware";
+// import { Endpoint, PayloadRequest } from "payload";
+
+// export const generateKeywordsEndpoint : Endpoint = {
+
+//     path : "/generateKeywords",
+
+//     method : "post",
+
+//     handler : withErrorHandling(async (req : PayloadRequest) => {
+
+//         const { category }
+//     })
+// }
