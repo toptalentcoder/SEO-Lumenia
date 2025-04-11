8a8ac9d10c3e35df09ac76992f41c5cd80a0b631
toptalentcoder <kevinhart.dev@gmail.com>
2025-04-11 14:34:08 -0400

    add useSearchView Hook
    Co-authored-by: HealerDevOps <gonggong624721@gmail.com>

---
+++ client/src/app/top/pages/page.js
@@ -0,0 +1,41 @@
+"use client"
+
+import { useState } from 'react';
+import { useSearchView } from '../../../hooks/useSearchView';
+
+export default function Linking(){
+
+    const {currentView, responseData, switchToResults, switchToInput } = useSearchView();
+    const [loading, setLoading] = useState(false);
+
+    const handleSearch = async (data) => {
+        setLoading(true); // Start loading animation
+
+        try {
+            // Store the response and show results
+            switchToResults(data);
+        } catch (error) {
+            console.error("Search failed:", error);
+        } finally {
+            setLoading(false); // Stop loading animation
+        }
+    };
+
+    return(
+        <div>
+            {loading ? (
+                <div>
+                    Loading....
+                </div>
+            ) : currentView === 'input' ? (
+                <div>
+                    InputView
+                </div>
+            ) : (
+                <div>
+                    ResultView
+                </div>
+            )}
+        </div>
+    )
+}
